b1dad466d9e1570c651d91fc33acbae6c6a26b25
Your Nametomoisempay@gmail.com <you@example.com>
2023-01-31 14:36:45 +0600

    edd css

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,7 +2,7 @@
     <div>
         <nav>
             <div class="navMainConteiner">
-                <div class="navLogo">
+                <div class="navLogoWrapper">
                     <h2> Жакшы Жол</h2>
                 </div>
                 <div class="navLinks">
@@ -13,7 +13,7 @@
                     <router-link to="#">Контакты</router-link>
                 </div>
                 <div class="navButton">
-                    <button>Пройти тестирование</button>
+                    <button class="button">Пройти тестирование</button>
                 </div>
             </div>
         </nav>
@@ -24,6 +24,70 @@ export default {
 
 }
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
+* {
+    padding: 0px;
+    margin: 0px;
+    color: black;
+    text-decoration: none;
+}
 
+nav {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+
+    .navMainConteiner {
+        width: 90%;
+        height: 100px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        .navLinks {
+            width: 40%;
+            display: flex;
+            justify-content: space-between;
+
+            a {
+                font-family: 'Raleway', sans-serif;
+                font-style: normal;
+                font-weight: 400;
+                font-size: 18px;
+                line-height: 21px;
+                color: #000000;
+            }
+
+        }
+
+        h2 {
+            font-family: 'Comfortaa', cursive;
+            font-style: normal;
+            font-weight: 700;
+            font-size: 36px;
+            line-height: 40px;
+            color: #000000;
+        }
+
+        .button {
+            box-sizing: border-box;
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            align-items: center;
+            padding: 20px;
+            gap: 10px;
+            background: #032660;
+            border: 3px solid #032660;
+            border-radius: 8px;
+
+            font-family: 'Raleway', sans-serif;
+            font-style: normal;
+            font-weight: 500;
+            font-size: 18px;
+            line-height: 21px;
+            color: #FFFFFF;
+        }
+    }
+}
 </style>
