19a8beca7ac4e75f2632886b2364d83c6f105565
Your Nametomoisempay@gmail.com <you@example.com>
2023-01-31 12:07:54 +0600

    structured

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,18 +1,29 @@
 <template >
     <div>
         <nav>
-            <h1>This is nav bar</h1>
+            <div class="navMainConteiner">
+                <div class="navLogo">
+                    <h2> Жакшы Жол</h2>
+                </div>
+                <div class="navLinks">
+                    <router-link to="#">Обо мне</router-link>
+                    <router-link to="#">Методичка</router-link>
+                    <router-link to="#">Видеоуроки</router-link>
+                    <router-link to="#">Отзывы</router-link>
+                    <router-link to="#">Контакты</router-link>
+                </div>
+                <div class="navButton">
+                    <button>Пройти тестирование</button>
+                </div>
+            </div>
         </nav>
     </div>
 </template>
 <script>
 export default {
-    
+
 }
 </script>
 <style lang="scss">
-    nav{
-        height: 100px;
-        background: papayawhip;
-    }
+
 </style>
