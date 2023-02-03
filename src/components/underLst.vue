fdc6ae872b099238476d55e152f17f0f553f0abd
Mansur82 <mansurkhakimov02@mail.ru>
2023-02-03 15:13:49 +0600

    something

---
+++ src/components/underLst.vue
@@ -0,0 +1,231 @@
+<template>
+    <section class="footer">
+        <div class="footer__container">
+
+         <div class="footer__inner">
+            <h2>Информация о тесте</h2>
+        </div>
+        
+        <div class="footer__text">
+            <p>Какой нибудь текст про тест</p>
+        </div>
+
+        <button class="btn">Пройти тестирование</button>
+
+        </div> <!-- /footer__container -->
+
+        <div class="footer__img">
+        <img src="../assets/images/pexels-yan-krukau-8199761 1.png" alt="">
+        </div>
+        <!-- <div class="footer__icon">
+            <img src="../assets/images/Group 27.png" alt="">
+        </div> -->
+
+    </section> <!-- /footer -->
+
+ <section class="wrappppper">
+    <div class="footer__block-2">
+
+        <div class="footer__title">
+        <h2>Как связаться со мной?</h2>
+    </div>
+
+    <div class="footer__text_2">
+        <p>Если остались вопросы, можете связаться со мной удобным вам способом</p>
+    </div>
+    
+    <button class="btn-2">Связаться</button>
+    </div>
+ </section>
+
+ <section class="end__footer">
+    <div class="end"></div>
+        <div class="end__items">
+            <div class="end__item">
+                <h2>Жакшы Жол</h2>
+                <p>2023, Все права защищены</p>
+                
+            </div>
+            <div class="end__item">
+                <ul class="end__it">
+                    <li>Обо мне</li>
+                    <li>Методичка</li>
+                    <li>Видеоуроки</li>
+                    <li>Отзывы</li>
+                    <li>Контакты</li>
+                </ul>
+            </div>
+            <div class="end__item">
+                <button>Связаться</button>
+            </div>
+        </div>
+
+ </section>
+
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+/* ------------------  footer block-1 --------------- */
+.footer{
+    display: flex;
+    justify-content: space-around;
+    margin: 160px 0 ;
+}
+
+.footer__inner{
+    font-size: 30px;
+    color: #032660;
+    font-weight: 600;
+    padding: 0;
+}
+.footer__text{
+    font-weight: 400;
+    font-size: 22px;
+    padding: 0;
+}
+.footer__container{
+    height: 370px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+}
+.btn{
+    width: 300px;
+    background: #032660;
+    border-radius: 8px;
+    padding: 12px;
+    font-weight: 500;
+    font-size: 18px;
+    color: #fff;
+}
+
+/* ------------------  footer block-2 --------------- */
+
+.footer__block-2{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    width: 1238px;
+    background: url(../assets/images/Rectangle.png) center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    padding-left: 6%;
+}
+
+.wrappppper{
+    display: flex;
+    justify-content: center;
+    height: 400px;
+}
+
+.footer__title{
+    color: #FFFFFF;
+    font-weight: 600;
+    font-size: 26px;
+}
+
+.footer__text_2{
+    color: #FFFFFF;
+    font-weight: 400;
+    font-size: 19px;
+    min-width: 27%;
+    max-width: 23%;
+}
+
+.btn-2{
+    color: #ffffff;
+    border: 3px solid #032660;
+    border-radius: 8px;
+    width: 126px;
+    min-height: 40px;
+    position: relative;
+    left: 0.1%;
+    transform: translate(-1%, 0);
+}
+
+/* --------------- end__footer----------------- */
+.end{
+    margin: 100px 0px 50px 0px;
+    border-bottom: 3px solid #032660;
+}
+
+.end__items{
+    display: flex;
+    justify-content: space-around;
+}
+
+.end__item{
+   align-items: center;
+}
+
+.end__it{
+    display: flex;
+    align-items: center;
+    gap: 20px 100px;
+    flex-flow: wrap;
+    width: 75%;
+    height: 19vh;
+    margin: 0 auto;
+}
+
+li{
+    list-style: none;
+}
+
+.end__item p{
+padding: 60px 0;
+}
+
+.end__item h2{
+    font-weight: 700;
+    font-size: 32px;
+    color: #032660;
+}
+
+.end__item button{
+    border: 3px solid #032660;
+    border-radius: 8px;
+    padding: 10px 27px;
+    font-weight: 500;
+    font-size: 18px;
+    color: #000
+}
+
+@media (max-width: 768px) { 
+  .footer { 
+    flex-direction: column; 
+    align-items: center; 
+  } 
+  .footer__inner { 
+    font-size: 22px; 
+  } 
+  .footer__text { 
+    font-size: 18px; 
+  } 
+  .footer__block-2 { 
+    width: 100%; 
+    padding-left: 2%; 
+  } 
+  .footer__text_2 { 
+    min-width: 100%; 
+    max-width: 100%; 
+  } 
+  .btn { 
+    width: 200px; 
+  } 
+  .end__it { 
+    width: 90%; 
+    height: auto; 
+  } 
+}
+</style>
+
+
+
